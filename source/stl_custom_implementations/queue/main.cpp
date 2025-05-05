cedbf26e0e8d848ed09fff1311ff632d8a82ee64
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-06 00:43:12 +0300

    Update main.cpp

--- source/stl_custom_implementations/queue/main.cpp
+++ source/stl_custom_implementations/queue/main.cpp
@@ -1,12 +1,12 @@
-#include "Queue.hpp"
+#include "MyQueue.hpp"
 
-int main() 
+int main()
 {
 
-    try 
+    try
     {
-        
-        Queue<int> q;
+
+        MyQueue<int> q;
 
         q.push(10);
         q.push(20);
@@ -17,13 +17,13 @@ int main()
         q.pop();
         std::cout << q.size() << " " << q.front() << std::endl;
 
-        Queue<int> qCopy = q;
+        MyQueue<int> qCopy = q;
         std::cout  << qCopy.size() << " " << qCopy.front() << std::endl;
 
         qCopy.push(40);
         std::cout << qCopy.size() << " " << qCopy.front() << std::endl;
 
-        Queue<int> qMoved = std::move(qCopy);
+        MyQueue<int> qMoved = std::move(qCopy);
         std::cout << std::boolalpha << qCopy.empty() << std::endl;
         std::cout << qMoved.size()<< " " << qMoved.front() << std::endl;
 
@@ -41,7 +41,7 @@ int main()
         std::cout << q.empty() << std::endl;
 
     }
-    catch (const std::exception& e) 
+    catch (const std::exception& e)
     {
 
         std::cout << e.what() << std::endl;
