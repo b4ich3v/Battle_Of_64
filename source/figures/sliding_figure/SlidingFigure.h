8ad8565de1fb4180da57362b52fbb40ba12ecb14
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-07 00:08:35 +0300

    Update SlidingFigure.h

--- source/figures/sliding_figure/SlidingFigure.h
+++ source/figures/sliding_figure/SlidingFigure.h
@@ -1,12 +1,12 @@
 #include "Figure.h"
 #pragma once
 
-class SlidingFigure: public Figure
+class SlidingFigure: public Figure 
 {
 protected:
 
-    MyVector<Position> generateSliding(const Board& board, const Position& from,
-        MyVector<std::pair<int, int>> directions) const;
+    MyVector<Position> generateSliding(const Board& board, const Position& from, 
+        MyVector<MyPair<int, int>> directions) const;
 
 public:
 
