4c1672b60afdd2b60ba12d5a68cd00ae143486ad
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-06 23:46:34 +0300

    Update Position.cpp

--- source/position/Position.cpp
+++ source/position/Position.cpp
@@ -1,9 +1,9 @@
 #include "Position.h"
 
-Position::Position(uint8_t row, uint8_t col):
+Position::Position(int8_t row, int8_t col):
     row(row), col(col) {}
 
-bool Position::operator == (Position const& other) const
+bool Position::operator == (Position const& other) const 
 {
 
     return row == other.row && col == other.col;
