a0337f565d548f125776234334bfe3eba8d69ed9
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-08 00:18:04 +0300

    Create Player.h

---
+++ source/players/player/Player.h
@@ -0,0 +1,22 @@
+#include "Move.h"
+#include "Board.h"
+#include "MyString.h"  
+#pragma once
+
+class Player
+{
+protected:
+
+    MyString name;
+
+public:
+    
+    explicit Player(const MyString& name);
+
+    virtual ~Player() = default;
+
+    const MyString& getName() const;
+
+    virtual Move requestMove(const Board& board, Color mover) = 0;
+
+};
