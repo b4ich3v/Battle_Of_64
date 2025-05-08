42cfb0c5c22767e8b72f118deb248b6197c25fb3
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-09 00:15:56 +0300

    Update Bishop.h

--- source/figures/bishop/Bishop.h
+++ source/figures/bishop/Bishop.h
@@ -1,13 +1,15 @@
 #include "SlidingFigure.h"
 #pragma once
 
-class Bishop: public SlidingFigure
+class Bishop: public SlidingFigure 
 {
 public:
 
     explicit Bishop(Color color);
 
     MyVector<Position> generateMoves(const Board& board, const Position& from) const override;
+
+    void accept(Visitor& visitor) const override;
 
     char symbol() const override;
 
