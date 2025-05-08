089624474c0bd589b329c49ae010f913f0016c01
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-09 00:17:38 +0300

    Update Knight.h

--- source/figures/knight/Knight.h
+++ source/figures/knight/Knight.h
@@ -1,13 +1,15 @@
 #include "Figure.h"
 #pragma once
 
-class Knight: public Figure
+class Knight: public Figure 
 {
 public:
 
     explicit Knight(Color color);
 
     MyVector<Position> generateMoves(const Board& board, const Position& from) const override;
+
+    void accept(Visitor& visitor) const override;
 
     char symbol() const override;
 
