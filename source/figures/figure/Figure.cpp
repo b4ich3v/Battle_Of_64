8b083b3a30ed9fd6a162bbd885d2f5c807a2d065
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-09 00:16:48 +0300

    Update Figure.cpp

--- source/figures/figure/Figure.cpp
+++ source/figures/figure/Figure.cpp
@@ -1,18 +1,18 @@
 #include "Figure.h"
 
-Figure::Figure(Color color, FigureType type):
+Figure::Figure(Color color, FigureType type): 
 	color(color), type(type) {}
 
-Color Figure::getColor() const
+Color Figure::getColor() const 
 {
 
-	return color;
+	return color; 
 
 }
 
-FigureType Figure::getType() const
-{
+FigureType Figure::getType() const 
+{ 
 
-	return type;
+	return type; 
 
 }
