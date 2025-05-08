d36e01fedaa26b6c3cc5e48a0039e05fef11948a
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-09 00:18:15 +0300

    Update Pawn.cpp

--- source/figures/pawn/Pawn.cpp
+++ source/figures/pawn/Pawn.cpp
@@ -4,7 +4,7 @@
 Pawn::Pawn(Color color):
     Figure(color, FigureType::PAWN) {}
 
-MyVector<Position> Pawn::generateMoves(Board const& board, Position const& from) const
+MyVector<Position> Pawn::generateMoves(Board const& board, Position const& from) const 
 {
 
     MyVector<Position> moves;
@@ -13,7 +13,7 @@ MyVector<Position> Pawn::generateMoves(Board const& board, Position const& from)
 
     if (board.isValid(one) && board.at(one) == nullptr)
     {
-
+        
         if ((color == Color::WHITE && one.row == 0) ||
             (color == Color::BLACK && one.row == 7))
         {
@@ -21,17 +21,17 @@ MyVector<Position> Pawn::generateMoves(Board const& board, Position const& from)
             moves.push_back(Move(from, one, SpecialMove::PROMOTION, FigureType::QUEEN).to);
 
         }
-        else
+        else 
         {
 
             moves.push_back(one);
 
         }
-
+        
         bool home = (color == Color::WHITE && from.row == 6) || (color == Color::BLACK && from.row == 1);
         Position two{ from.row + 2 * dir, from.col };
 
-        if (home && board.isValid(two) && board.at(two) == nullptr)
+        if (home && board.isValid(two) && board.at(two) == nullptr) 
         {
 
             moves.push_back(two);
@@ -39,7 +39,7 @@ MyVector<Position> Pawn::generateMoves(Board const& board, Position const& from)
         }
 
     }
-
+    
     for (int dc = -1; dc <= 1; dc += 2)
     {
 
@@ -48,15 +48,15 @@ MyVector<Position> Pawn::generateMoves(Board const& board, Position const& from)
         if (!board.isValid(cap)) continue;
 
         Figure* occ = board.at(cap);
-
-        if (occ && occ->getColor() != color)
+        
+        if (occ && occ->getColor() != color) 
         {
-
+        
             if ((color == Color::WHITE && cap.row == 0) ||
                 (color == Color::BLACK && cap.row == 7))
             {
 
-                moves.push_back(cap);
+                moves.push_back(cap); 
 
             }
             else
@@ -67,20 +67,20 @@ MyVector<Position> Pawn::generateMoves(Board const& board, Position const& from)
             }
 
         }
-
+        
         Position behind{ from.row, from.col + dc };
         Figure* adj = board.isValid(behind) ? board.at(behind) : nullptr;
 
         if (adj && adj->getType() == FigureType::PAWN && adj->getColor() != color)
         {
-
+            
             auto last = board.peekHistory();
 
             if (last.move.special == SpecialMove::NORMAL &&
                 last.move.to == behind &&
-                abs(last.move.from.row - last.move.to.row) == 2)
+                abs(last.move.from.row - last.move.to.row) == 2) 
             {
-
+                
                 moves.push_back(cap);
 
             }
@@ -93,7 +93,14 @@ MyVector<Position> Pawn::generateMoves(Board const& board, Position const& from)
 
 }
 
-char Pawn::symbol() const
+void Pawn::accept(Visitor& visitor) const
+{
+
+    visitor.visit(*this);
+
+}
+
+char Pawn::symbol() const 
 {
 
     return (color == Color::WHITE ? 'P' : 'p');
