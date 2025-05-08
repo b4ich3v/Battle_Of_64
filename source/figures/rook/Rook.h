ecb3309cc80f717844b3e005dc1fc837530b56c0
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-09 00:19:06 +0300

    Update Rook.h

--- source/figures/rook/Rook.h
+++ source/figures/rook/Rook.h
@@ -1,13 +1,15 @@
 #include "SlidingFigure.h"
 #pragma once
 
-class Rook: public SlidingFigure
+class Rook: public SlidingFigure 
 {
 public:
 
     explicit Rook(Color color);
 
     MyVector<Position> generateMoves(const Board& board, const Position& from) const override;
+
+    void accept(Visitor& visitor) const override;
 
     char symbol() const override;
 
