b7254832b4604d102a4c6470af9324a809727c3a
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-09 00:11:17 +0300

    Create Visitor.h

---
+++ source/visitors/visitor/Visitor.h
@@ -0,0 +1,31 @@
+#pragma once
+
+class Board;
+class Rook;
+class Knight;
+class Bishop;
+class Queen;
+class King;
+class Pawn;
+
+class Visitor 
+{
+public:
+
+    virtual void visit(const Board& board) = 0;
+
+    virtual void visit(const Rook& rook) = 0;
+
+    virtual void visit(const Knight& knight) = 0;
+
+    virtual void visit(const Bishop& bishop) = 0;
+
+    virtual void visit(const Queen& queen) = 0;
+
+    virtual void visit(const King& king) = 0;
+
+    virtual void visit(const Pawn& pawn) = 0;
+
+    virtual ~Visitor() = default;
+
+};
