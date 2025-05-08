070cf3f1e811e75b9f3f0d66a73dc952d9efbc81
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-09 00:17:05 +0300

    Update King.h

--- source/figures/king/King.h
+++ source/figures/king/King.h
@@ -1,13 +1,15 @@
 #include "Figure.h"
 #pragma once
 
-class King: public Figure
+class King: public Figure 
 {
 public:
 
     explicit King(Color color);
 
     MyVector<Position> generateMoves(const Board& board, const Position& from) const override;
+
+    void accept(Visitor& visitor) const override;
 
     char symbol() const override;
 
