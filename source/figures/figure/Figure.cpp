63399cb8208b0cd50af235147ecfebf658e16e7e
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-13 23:44:19 +0300

    Update Figure.cpp

--- source/figures/figure/Figure.cpp
+++ source/figures/figure/Figure.cpp
@@ -16,3 +16,17 @@ FigureType Figure::getType() const
 	return type; 
 
 }
+
+Position Figure::getPosition() const 
+{
+
+	return position;
+
+}
+
+void Figure::setPosition(const Position& position) 
+{
+
+	this->position = position;
+
+}
