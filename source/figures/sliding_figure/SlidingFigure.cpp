28529821a5c1bc0c6def8703c1d416ee679f4ec3
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-13 23:47:02 +0300

    Update SlidingFigure.cpp

--- source/figures/sliding_figure/SlidingFigure.cpp
+++ source/figures/sliding_figure/SlidingFigure.cpp
@@ -1,51 +1,49 @@
 #include "SlidingFigure.h"
 #include "Board.h"
 
-SlidingFigure::SlidingFigure(Color color, FigureType type)
-    : Figure(color, type) {}
+SlidingFigure::SlidingFigure(Color color, FigureType type): 
+    Figure(color, type) {}
 
-MyVector<Position> SlidingFigure::generateSliding(const Board& board, const Position& from,
+MyVector<Move> SlidingFigure::generateSliding(const Board& board, const Position& from,
     const MyVector<MyPair<int, int>>& directions) const
 {
 
-    MyVector<Position> moves;
-    size_t directionCount = directions.size();
+    MyVector<Move> result;
 
-    for (size_t i = 0; i < directionCount; i++) 
+    for (std::size_t i = 0; i < directions.size(); i++)
     {
 
-        int dx = directions[i].first;
-        int dy = directions[i].second;
+        int deltaRow = directions[i].first;
+        int deltaCol = directions[i].second;
 
-        for (int step = 1; step < 8; step++)
+        Position currentPosition = from;
+
+        while (true) 
         {
 
-            Position to;
+            currentPosition.row += (int8_t)(deltaRow);
+            currentPosition.col += (int8_t)(deltaCol);
 
-            to.row = from.row + dx * step;
-            to.col = from.col + dy * step;
+            if (!board.isValid(currentPosition)) break;
 
-            if (!board.isValid(to)) break;
-                
+            const Figure* target = board.at(currentPosition);
 
-            Figure* occupied = board.at(to);
+            if (target == nullptr || target->getColor() != getColor())
+                result.push_back(Move(from, currentPosition));   
 
-            if (!occupied) 
-            {
-
-                moves.push_back(to);
-                continue;
-
-            }
-
-            if (occupied->getColor() != this->getColor()) moves.push_back(to);
-        
-            break;
-
+            if (target != nullptr) break;           
         }
 
     }
 
-    return moves;
+    return result;
+
+}
+
+MyVector<Move> SlidingFigure::generateMoves(const Board&,
+    const Position&) const
+{
+
+    return {};   
 
 }
