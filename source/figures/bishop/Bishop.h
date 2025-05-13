de93594abd193303bb379fdbffcedd772ed6edfe
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-13 23:43:36 +0300

    Update Bishop.h

--- source/figures/bishop/Bishop.h
+++ source/figures/bishop/Bishop.h
@@ -7,7 +7,7 @@ public:
 
     explicit Bishop(Color color);
 
-    MyVector<Position> generateMoves(const Board& board, const Position& from) const override;
+    MyVector<Move> generateMoves(const Board& board, const Position& from) const override;
 
     void accept(Visitor& visitor) const override;
 
