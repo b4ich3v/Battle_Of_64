667063b5a110879076cbbb0d16c8385c2f6870ff
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-13 23:46:53 +0300

    Update SlidingFigure.h

--- source/figures/sliding_figure/SlidingFigure.h
+++ source/figures/sliding_figure/SlidingFigure.h
@@ -1,16 +1,17 @@
-#include "Figure.h"
 #pragma once
+#include "Figure.h"
 
-class SlidingFigure: public Figure 
+class SlidingFigure: public Figure
 {
 protected:
-
-    MyVector<Position> generateSliding(const Board& board, const Position& from, 
-        const MyVector<MyPair<int, int>>& directions) const;
+ 
+    MyVector<Move>generateSliding(const Board& board, const Position& from,
+            const MyVector<MyPair<int, int>>& directions) const;
 
 public:
 
     SlidingFigure(Color color, FigureType type);
 
-};
+    MyVector<Move> generateMoves(const Board& board, const Position& from) const override;
 
+};
