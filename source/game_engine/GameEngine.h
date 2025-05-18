5ddff17cd20b59828ba75792781cb5686aa1f2f6
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-18 22:32:43 +0300

    Update GameEngine.h

--- source/game_engine/GameEngine.h
+++ source/game_engine/GameEngine.h
@@ -1,1 +1,58 @@
+#define UNICODE
+#define _UNICODE
+#include <windows.h>
+#include <gdiplus.h>
+#include "VisitorVisualization.h"
+#include "Board.h"
+#include "Move.h"
+#include "Position.h"
+#pragma once
+
+class GameEngine 
+{
+private:
+
+    VisitorVisualization* vizualizator = nullptr;
+
+    Gdiplus::Image* gif = nullptr;
+    GUID dimID;
+    UINT frameCount, currentFrame;
+    UINT* delays = nullptr;
+    UINT timerID;
+
+    HINSTANCE hInst;
+    HWND hMainWnd;
+    HWND hChessWnd;
+
+    bool dragging = 0;
+    int drag_from_row, drag_from_col = 0;
+    int drag_mouse_x, drag_mouse_y = 0;
+    Piece drag_piece;
+    MyColor currentTurn;
+
+    GameEngine();
+
+    ~GameEngine();
+
+    ULONG_PTR gdipToken{ 0 };
+
+    static LRESULT CALLBACK MainWndProc(HWND, UINT, WPARAM, LPARAM);
+
+    static LRESULT CALLBACK ChessWndProc(HWND, UINT, WPARAM, LPARAM);
+
+    void initGifWindow();
+
+    void initChessWindow();
+
+    void paintChess(HWND hWnd, HDC hdc);
+
+    FigureType askPromotion(HWND parent);
+
+public:
+
+    static GameEngine& instance();
+
+    int run(HINSTANCE hInst, int nShow);
+
+};
 
