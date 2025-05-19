d2812ef6432267b4de5de31c3a7dd973f500d052
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-20 00:25:32 +0300

    Update Reader.h

--- source/serializer_deserializer/reader/Reader.h
+++ source/serializer_deserializer/reader/Reader.h
@@ -1,1 +1,17 @@
+#include <iostream>
+#pragma once
+
+class Reader
+{
+public:
+
+    virtual bool open(const char* fileName) = 0;
+
+    virtual bool read(void* desination, size_t bytes) = 0;
+
+    virtual void close() = 0;
+
+    virtual ~Reader() = default;
+
+};
 
