e6d08512a10154c0263e18e404420a8358963905
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-10 21:39:07 +0300

    Update Board.cpp

--- source/board/Board.cpp
+++ source/board/Board.cpp
@@ -6,17 +6,6 @@
 #include "Pawn.h"
 #include "Board.h"
 
-HistoryEntry::HistoryEntry() :
-    move(Position{ 0,0 }, Position{ 0,0 }), captured(nullptr) {}
-
-HistoryEntry::HistoryEntry(const Move& move, Figure* captured) :
-    move(move), captured(captured) {}
-
-HistoryEntry::HistoryEntry(const Move& move, Figure* captured,
-    const MyVector<bool>& prevKing, const MyVector<bool>& prevQueen):
-    move(move), captured(captured),
-    castleKingSide(prevKing), castleQueenSide(prevQueen) {}
-
 Board::Board(): table(), history()
 {
 
