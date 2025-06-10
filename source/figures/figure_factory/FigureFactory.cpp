25a808ffc2e8d8e3b12aa779aec12469e2277003
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-10 22:29:25 +0300

    Create FigureFactory.cpp

---
+++ source/figures/figure_factory/FigureFactory.cpp
@@ -0,0 +1,27 @@
+#include "FigureFactory.h"
+#include "Pawn.h"
+#include "Rook.h"
+#include "Knight.h"
+#include "Queen.h"
+#include "King.h"
+#include "Bishop.h"
+
+Figure* FigureFactory::create(FigureType type, MyColor color) 
+{
+
+    switch (type) 
+    {
+
+    case FigureType::PAWN: return new Pawn(color);
+    case FigureType::KNIGHT: return new Knight(color);
+    case FigureType::BISHOP: return new Bishop(color);
+    case FigureType::ROOK: return new Rook(color);
+    case FigureType::QUEEN: return new Queen(color);
+    case FigureType::KING: return new King(color);
+    default: return nullptr;
+
+    }
+
+    return nullptr;
+
+}
