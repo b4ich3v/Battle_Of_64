1917960b082f82556f0e55823bea25e649f589ee
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-10 23:07:02 +0300

    Create Types.h

---
+++ source/types/Types.h
@@ -0,0 +1,79 @@
+#include <iostream>
+#pragma once
+
+static constexpr int ROWS_COUNT = 8;
+static constexpr int COLS_COUNT = 8;
+static constexpr int BOARD_SIZE = 8;
+
+static constexpr int8_t INVALID_ROW = -1;
+static constexpr int8_t INVALID_COL = -1;
+
+static constexpr int8_t KING_START_COL = 4;
+
+static constexpr int8_t KS_VIA_COL = 5;
+static constexpr int8_t QS_VIA_COL = 3;
+
+static constexpr int8_t KS_ROOK_FROM_COL = 7;
+static constexpr int8_t KS_ROOK_TO_COL = 5;
+static constexpr int8_t QS_ROOK_FROM_COL = 0;
+static constexpr int8_t QS_ROOK_TO_COL = 3;
+
+static constexpr int8_t EP_ROW_WHITE = 3;
+static constexpr int8_t EP_ROW_BLACK = 4;
+
+static constexpr int8_t EP_DIR_WHITE = -1;
+static constexpr int8_t EP_DIR_BLACK = +1;
+
+static constexpr int WHITE_BACK_ROW = BOARD_SIZE - 1;
+static constexpr int WHITE_PAWN_ROW = BOARD_SIZE - 2;
+static constexpr int BLACK_BACK_ROW = 0;
+static constexpr int BLACK_PAWN_ROW = 1;
+
+static constexpr int KS_EMPTY_COL1 = 5; 
+static constexpr int KS_EMPTY_COL2 = 6; 
+
+static constexpr int QS_EMPTY_COL1 = 1; 
+static constexpr int QS_EMPTY_COL2 = 2; 
+static constexpr int QS_EMPTY_COL3 = 3; 
+
+enum class MyColor: uint8_t
+{
+
+    WHITE,
+    BLACK
+
+};
+
+enum class FigureType: uint8_t
+{
+
+    NONE,
+    PAWN,
+    KNIGHT,
+    BISHOP,
+    ROOK,
+    QUEEN,
+    KING
+
+};
+
+static constexpr FigureType backrank[BOARD_SIZE] =
+{
+
+    FigureType::ROOK, FigureType::KNIGHT, FigureType::BISHOP,
+    FigureType::QUEEN, FigureType::KING, FigureType::BISHOP,
+    FigureType::KNIGHT, FigureType::ROOK
+
+};
+
+enum class SpecialMove: uint8_t
+{
+
+    NORMAL,
+    CASTLING_KING_SIDE,
+    CASTLING_QUEEN_SIDE,
+    EN_PASSANT,
+    PROMOTION,
+    DOUBLE_PAWN
+
+};
