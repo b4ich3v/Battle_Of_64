800f8684097f88b92ea2ce21219370061375d367
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-11 00:13:43 +0300

    Update Rook.cpp

--- source/figures/rook/Rook.cpp
+++ source/figures/rook/Rook.cpp
@@ -1,22 +1,6 @@
 #include "Rook.h"
 
-static MyVector<MyPair<int, int>> makeRookDirs()
-{
-
-    MyVector<MyPair<int, int>> dirs;
-
-    dirs.push_back(MyPair<int, int>(-1, 0));   
-    dirs.push_back(MyPair<int, int>(1, 0));   
-    dirs.push_back(MyPair<int, int>(0, -1));   
-    dirs.push_back(MyPair<int, int>(0, 1));   
-
-    return dirs;
-
-}
-
-static const MyVector<MyPair<int, int>> ROOK_DIRS = makeRookDirs();
-
-Rook::Rook(Color color): 
+Rook::Rook(MyColor color): 
     SlidingFigure(color, FigureType::ROOK) {}
 
 MyVector<Move> Rook::generateMoves(const Board& board,
@@ -37,6 +21,6 @@ void Rook::accept(Visitor& visitor) const
 char Rook::symbol() const
 {
 
-    return (getColor() == Color::WHITE ? 'R' : 'r');
+    return (getColor() == MyColor::WHITE ? 'R' : 'r');
 
 }
