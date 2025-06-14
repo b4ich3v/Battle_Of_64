feeef3edb929251d742fbe343babfe9fe047c879
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-14 21:58:15 +0300

    Update Writer.h

--- source/serializer_deserializer/writer/Writer.h
+++ source/serializer_deserializer/writer/Writer.h
@@ -5,11 +5,11 @@ class Writer
 {
 public:
 
-    virtual bool open(const char* fileName) = 0;
+    virtual bool open(const char* fileName) = 0; // open file for writing; truncate if it exists
 
-    virtual bool write(const void* source, size_t bytes) = 0;
+    virtual bool write(const void* source, size_t bytes) = 0; // write "bytes" from "source", return true if all bytes written
 
-    virtual void close() = 0;
+    virtual void close() = 0; // flush and close handle
 
     virtual ~Writer() = default;
 
