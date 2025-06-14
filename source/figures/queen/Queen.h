fc894be9cad4968b9f053eeb92ec8c94abc8b19b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-14 21:33:09 +0300

    Update Queen.h

--- source/figures/queen/Queen.h
+++ source/figures/queen/Queen.h
@@ -5,13 +5,13 @@ class Queen: public SlidingFigure
 {
 public:
 
-    explicit Queen(MyColor color);
+    explicit Queen(MyColor color); // just forwards color
 
-    MyVector<Move> generateMoves(const Board& board,const Position& from) const override;
+    MyVector<Move> generateMoves(const Board& board,const Position& from) const override; // QUEEN_DIRS
 
     void accept(Visitor& visitor) const override;
 
-    char symbol() const override;
+    char symbol() const override; // 'Q', 'q'
 
 };
 
