fc4857dd018df0b029d0b92291d435c13a4ea5c1
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-14 21:43:56 +0300

    Update Position.h

--- source/position/Position.h
+++ source/position/Position.h
@@ -5,12 +5,12 @@ struct Position
 {
 public:
 
-    int8_t row = 0; 
-    int8_t col = 0; 
+    int8_t row = 0; // 0-7, rank from White’s side
+    int8_t col = 0; // 0-7, file a-h
 
     Position(int8_t row = 0, int8_t col = 0);
 
-    bool operator == (Position const& other) const;
+    bool operator == (Position const& other) const; // exact same square
 
 };
 
