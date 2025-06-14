23f9b1315e8ecc302b8bebbe2633942e3933a9f2
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-14 21:58:03 +0300

    Update Reader.h

--- source/serializer_deserializer/reader/Reader.h
+++ source/serializer_deserializer/reader/Reader.h
@@ -5,12 +5,12 @@ class Reader
 {
 public:
 
-    virtual bool open(const char* fileName) = 0;
+    virtual bool open(const char* fileName) = 0; // open file for reading; return true on success
 
-    virtual bool read(void* desination, size_t bytes) = 0;
+    virtual bool read(void* desination, size_t bytes) = 0; // read "bytes" into "destination", return true if all bytes read
 
-    virtual void close() = 0;
+    virtual void close() = 0; // close underlying handle (safe-to-call twice)
 
-    virtual ~Reader() = default;
+    virtual ~Reader() = default; // polymorphic destruction
 
 };
