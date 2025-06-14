0338c816d48549e16cf917577680365bff87367d
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-14 22:00:17 +0300

    Update BinaryReader.cpp

--- source/serializer_deserializer/binary_reader/BinaryReader.cpp
+++ source/serializer_deserializer/binary_reader/BinaryReader.cpp
@@ -3,7 +3,7 @@
 bool BinaryReader::open(const char* fileName) 
 {
 
-    file.open(fileName, std::ios::binary); 
+    file.open(fileName, std::ios::binary); // open read-only
     return file.is_open();
 
 }
@@ -11,13 +11,13 @@ bool BinaryReader::open(const char* fileName)
 bool BinaryReader::read(void* destination, size_t bytes) 
 {
 
-    return bool(file.read((char*)(destination), bytes));
+    return bool(file.read((char*)(destination), bytes)); // true on full read
 
 }
 
 void BinaryReader::close() 
 { 
 
-    if (file.is_open()) file.close(); 
+    if (file.is_open()) file.close(); // idempotent
 
 }
