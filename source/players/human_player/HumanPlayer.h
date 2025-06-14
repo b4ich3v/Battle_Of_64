1306b0047fcbdd092500892ff31b4a0ee527109f
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-14 21:11:31 +0300

    Update HumanPlayer.h

--- source/players/human_player/HumanPlayer.h
+++ source/players/human_player/HumanPlayer.h
@@ -5,8 +5,8 @@ class HumanPlayer: public Player
 {
 public:
 
-    Move getMove(Board& board, MyColor color) override;
+    Move getMove(Board& board, MyColor color) override; // deliver the move that the GUI, user has just entered
 
-    void setPendingMove(const Move& move) override;
+    void setPendingMove(const Move& move) override; // called by the UI to pass the user’s move in
 
 };
