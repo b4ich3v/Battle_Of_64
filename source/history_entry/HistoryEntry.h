01486743e57356bbd8c4462d60294a1deb03df2b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-14 21:54:08 +0300

    Update HistoryEntry.h

--- source/history_entry/HistoryEntry.h
+++ source/history_entry/HistoryEntry.h
@@ -6,17 +6,18 @@ struct HistoryEntry
 {
 public:
 
-    Move move;
-    Figure* captured;
+    Move move; // move just played
+    Figure* captured; // piece captured (may be null)
 
+    // castling rights "before" the move-for undo
     MyVector<bool> castleKingSide;
     MyVector<bool> castleQueenSide;
 
-    HistoryEntry();
+    HistoryEntry(); // empty
 
-    HistoryEntry(const Move& move, Figure* captured);
+    HistoryEntry(const Move& move, Figure* captured); // no castles
 
-    HistoryEntry(const Move& move, Figure* captured,
+    HistoryEntry(const Move& move, Figure* captured, // full info
         const MyVector<bool>& prevKing, const MyVector<bool>& prevQueen);
 
 };
