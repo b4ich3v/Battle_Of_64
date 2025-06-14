e0f1b96dbad2c486545ee5d3496d3b708ceccac7
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-14 21:54:17 +0300

    Update HistoryEntry.cpp

--- source/history_entry/HistoryEntry.cpp
+++ source/history_entry/HistoryEntry.cpp
@@ -1,12 +1,12 @@
 #include "HistoryEntry.h"
 
-HistoryEntry::HistoryEntry():
+HistoryEntry::HistoryEntry(): // dummy entry
     move(Position{ 0,0 }, Position{ 0,0 }), captured(nullptr) {}
 
-HistoryEntry::HistoryEntry(const Move& move, Figure* captured):
+HistoryEntry::HistoryEntry(const Move& move, Figure* captured): // no castles
     move(move), captured(captured) {}
 
 HistoryEntry::HistoryEntry(const Move& move, Figure* captured,
-    const MyVector<bool>& prevKing, const MyVector<bool>& prevQueen) :
+    const MyVector<bool>& prevKing, const MyVector<bool>& prevQueen): // full undo data
     move(move), captured(captured),
     castleKingSide(prevKing), castleQueenSide(prevQueen) {}
