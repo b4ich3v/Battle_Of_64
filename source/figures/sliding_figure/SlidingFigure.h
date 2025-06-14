80e8ac25ed25f61984cd6f6b4207967cff17c509
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-14 21:40:01 +0300

    Update SlidingFigure.h

--- source/figures/sliding_figure/SlidingFigure.h
+++ source/figures/sliding_figure/SlidingFigure.h
@@ -5,13 +5,15 @@ class SlidingFigure: public Figure
 {
 protected:
  
-    MyVector<Move>generateSliding(const Board& board, const Position& from,
+    // generic N-direction ray-tracing helper (used by rook, bishop, queen)
+    MyVector<Move>generateSliding(const Board& board, const Position& from, 
             const MyVector<MyPair<int, int>>& directions) const;
 
 public:
 
     SlidingFigure(MyColor color, FigureType type);
 
+    // base override does nothing-real pieces override again
     MyVector<Move> generateMoves(const Board& board, const Position& from) const override;
 
 };
